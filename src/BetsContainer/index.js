88eaab67b710f5a43e91d354b7a36829e20df707
Randy Gohmann <rgohma2@gmail.com>
2020-09-23 20:24:23 -0500

    added BetsContainer and rendered it

---
+++ src/BetsContainer/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+class BetsContainer extends React.Component {
+	constructor(props) {
+
+		super(props)
+
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return(
+			<h2 style={{backgroundColor: 'blue'}}>BetsContainer</h2>
+		)
+	}
+}
+
+export default BetsContainer
